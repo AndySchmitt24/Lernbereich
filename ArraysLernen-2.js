14019a1da6649b90ff90e5c6e01e0324c47d1dd6
Andy <andyschmitt24@gmail.com>
2023-09-26 16:51:05 +0200

    Add new tasks

---
+++ ArraysLernen-2.js
@@ -0,0 +1,21 @@
+const users = require("./users-subset.json");
+
+// 1. Create an array that contains only users younger than 50
+
+const usersYoungerThan50 = []; // replace the empty array with your code!
+console.log("usersYoungerThan50", usersYoungerThan50);
+
+// 2. Create an array that contains only users that love gaming
+const usersWhoLoveGaming = []; // replace the empty array with your code!
+console.log("usersWhoLoveGaming", usersWhoLoveGaming);
+
+// 3. Create a new array with female users that love gaming. Sort the array by age.
+
+const femaleGamersOrderedByAge = [];
+console.log("femaleGamersOrderedByAge", femaleGamersOrderedByAge);
+
+// 4. Create an array with users without hobbies. The Array should only consist of the names of the users like this:
+// ["John Doe", "Jane Foo"]
+
+const boringUsers = []; // replace the empty array with your code!
+console.log("boringUsers", boringUsers);
